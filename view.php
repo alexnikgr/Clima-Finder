e21bf8b45b920af8b5865652310bf0e95c4c7242
alexnikgr <67170274+alexnikgr@users.noreply.github.com>
2026-05-01 20:00:13 +0300

    Create master layout in view.php
    Added master layout to stitch together modular components.

---
+++ view.php
@@ -0,0 +1,19 @@
+<?php 
+/**
+ * Master Layout
+ * Stitching together the modular components.
+ */
+
+// 1. Meta tags and Dynamic CSS
+include 'header.php'; 
+
+// 2. Results, BTU Hero, and Technical Report
+include 'results_view.php'; 
+
+// 3. User Input Form and Settings
+include 'form_view.php'; 
+
+?>
+</div> <!-- Close .grid from header -->
+</body>
+</html>
